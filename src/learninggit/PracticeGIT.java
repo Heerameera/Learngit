a3783d1f907ff74ed6dbc3950c9ad6f5c82d56b4
MeeraHeera <gowthamiheera06@gmail.com>
2023-03-15 12:32:37 +0530

    changed commit

--- src/learninggit/PracticeGIT.java
+++ src/learninggit/PracticeGIT.java
@@ -4,6 +4,9 @@ public class PracticeGIT {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to git");
+		System.out.println("hi iam gowthami");
+		
+		System.out.println("push to new branch");
 	}
 
 }
